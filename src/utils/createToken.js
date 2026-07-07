46527c5f7c30b25aeb289700e82d327ea360066c
DavidKAblee <sirdavidudoji07@gmail.com>
2026-07-07 19:11:50 +0100

    CICD

--- src/utils/createToken.js
+++ src/utils/createToken.js
@@ -5,7 +5,7 @@ const createToken = (userId) => {
     process.env.JWT_SECRET ||
     'a9597f305350a847503b7d86967d731a401ffa32ce21320f6ced2c929f143dcb2f05f23dcacabdf0ca12ce84f9e598aa6b86c8c83ce0c04bf64e585e18b03f3c';
 
-  return jwt.sign({ id: userId }, secret, { expiresIn: '30d' });
+  return jwt.sign({ id: userId }, secret, { expiresIn: '1y' });
 };
 
 export default createToken;
